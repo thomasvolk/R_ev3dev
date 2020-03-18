e611969e145f9c840d55137237e6cb6b93123958
Thomas Volk (ThinkPad) <thomas@my-volk.de>
2020-03-18 22:02:00 +0100

    fix imports

--- R_ev3dev/__init__.py
+++ R_ev3dev/__init__.py
@@ -3,6 +3,7 @@ from R_ev3dev.motor import Tank
 from R_ev3dev.sensor import Color
 from R_ev3dev.server import Server
 from R_ev3dev.ev3 import ev3dev2
+from ev3dev2 import motor, sensor
 import logging, os
 
 
@@ -13,14 +14,14 @@ class NoOperation(Command):
 
 def ev3_interpreter():
     return Interpreter([
-        Reference('A', ev3dev2.motor.OUTPUT_A),
-        Reference('B', ev3dev2.motor.OUTPUT_B),
-        Reference('C', ev3dev2.motor.OUTPUT_C),
-        Reference('D', ev3dev2.motor.OUTPUT_D),
-        Reference('1', ev3dev2.sensor.INPUT_1),
-        Reference('2', ev3dev2.sensor.INPUT_2),
-        Reference('3', ev3dev2.sensor.INPUT_3),
-        Reference('4', ev3dev2.sensor.INPUT_4),
+        Reference('A', motor.OUTPUT_A),
+        Reference('B', motor.OUTPUT_B),
+        Reference('C', motor.OUTPUT_C),
+        Reference('D', motor.OUTPUT_D),
+        Reference('1', sensor.INPUT_1),
+        Reference('2', sensor.INPUT_2),
+        Reference('3', sensor.INPUT_3),
+        Reference('4', sensor.INPUT_4),
         Tank("tank"),
         Color("color"),
         NoOperation("hello")
