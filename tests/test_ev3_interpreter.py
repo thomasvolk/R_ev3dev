4bea13ee9876b248a3a71a2396d2ee154e35ba0f
Thomas Volk (ThinkPad) <thomas@my-volk.de>
2020-04-01 21:44:08 +0200

    add test for close

--- tests/test_ev3_interpreter.py
+++ tests/test_ev3_interpreter.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 import unittest
 from R_ev3dev import ev3_interpreter
+from R_ev3dev.server import CloseException
 
 
 class TestEv3Interpreter(unittest.TestCase):
     def test_hello(self):
         i = ev3_interpreter()
         self.assertEqual(i.evaluate("hello"), "ok")
+
+    def test_close(self):
+        i = ev3_interpreter()
+        self.assertRaises(CloseException, i.evaluate, "close")
