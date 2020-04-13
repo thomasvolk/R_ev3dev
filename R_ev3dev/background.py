f0781d5dd54a37fdf2e0c070ff1dd14808bf9e03
Thomas Volk (ThinkPad) <thomas@my-volk.de>
2020-04-13 15:13:11 +0200

    add first untested implementation

--- R_ev3dev/background.py
+++ R_ev3dev/background.py
@@ -1,6 +1,7 @@
 from queue import Queue
 from threading import Thread
 from enum import Enum
+from R_ev3dev.interpreter import Command
 
 
 class Signal(Enum):
@@ -43,14 +44,41 @@ class BackgroundRunner(object):
     def run_later(self, func, *args):
         if not callable(func):
             raise UnknownMessageException("unknown message: {}".format(func))
-        if not self.__queue:
+        if self.is_stopped():
             raise StoppedException("runner is stopped")
         self.__queue.put(Function(func, args))
 
+    def is_stopped(self):
+        return not self.__queue
+
     def stop(self):
-        if not self.__queue:
+        if self.is_stopped():
             raise StoppedException("runner is already stopped")
         self.__queue.put(Signal.POISON_PILL)
         self.__thread.join()
         self.__queue = None
         self.__thread = None
+
+
+class ToBackground(Command):
+    """ run command in the background
+
+        bg <command string> - runs the command in the background
+        bg                  - wait until all background processes are complete
+    """
+    def __init__(self, name):
+        super().__init__(name)
+        self.__background_runner = None
+
+    def invoke(self, interpreter_context, args):
+        if self.__background_runner is None:
+            self.__background_runner = BackgroundRunner()
+        if len(args):
+            command_string = " ".join(args)
+            self.__background_runner.run_later(
+                interpreter_context.interpreter.interpreter.evaluate,
+                command_string
+            )
+        else:
+            self.__background_runner.stop()
+            self.__background_runner = None
